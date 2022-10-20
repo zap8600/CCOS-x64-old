a50dc3f0472276f000d6648506ab33da55c39e70
zap8600 <95487746+zap8600@users.noreply.github.com>
2022-10-20 18:28:55 +0000

    new file:   build.sh 	new file:   clean.sh 	new file:   config.sh 	new file:   default-host.sh 	new file:   headers.sh 	new file:   iso.sh 	new file:   kernel/.gitignore 	new file:   kernel/Makefile 	new file:   kernel/arch/x86_64/crti.S 	new file:   kernel/arch/x86_64/crtn.S 	new file:   kernel/arch/x86_64/make.config 	new file:   kernel/arch/x86_64/tty.c 	new file:   kernel/include/kernel/bootboot.h 	new file:   kernel/include/kernel/ssfn.h 	new file:   kernel/include/kernel/tty.h 	new file:   kernel/kernel/kernel.c 	new file:   kernel/unifont.sfn 	new file:   libc/.gitignore 	new file:   libc/Makefile 	new file:   libc/arch/x86_64/make.config 	new file:   libc/include/stdio.h 	new file:   libc/include/stdlib.h 	new file:   libc/include/string.h 	new file:   libc/include/sys/cdefs.h 	new file:   libc/stdio/printf.c 	new file:   libc/stdio/putchar.c 	new file:   libc/stdio/puts.c 	new file:   libc/stdlib/abort.c 	new file:   libc/string/memcmp.c 	new file:   libc/string/memcpy.c 	new file:   libc/string/memmove.c 	new file:   libc/string/memset.c 	new file:   libc/string/strlen.c 	new file:   qemu.sh 	new file:   target-triplet-to-arch.sh

---
+++ kernel/arch/x86_64/tty.c
@@ -0,0 +1,42 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+#define SSFN_CONSOLEBITMAP_TRUECOLOR        /* use the special renderer for 32 bit truecolor packed pixels */
+#include <kernel/ssfn.h>
+#include <kernel/tty.h>
+
+extern uint8_t fb;
+extern volatile unsigned char _binary_unifont_sfn_start;
+
+void terminal_initialize(void) {
+   ssfn_src = &_binary_unifont_sfn_start;      /* the bitmap font to use */
+   ssfn_dst.ptr = &fb;                  /* framebuffer address and bytes per line */
+   ssfn_dst.p = 4096;
+   ssfn_dst.fg = 0xFFFFFFFF;                   /* colors, white on black */
+   ssfn_dst.bg = 0;
+   ssfn_dst.x = 100;                           /* coordinates to draw to */
+   ssfn_dst.y = 200;
+}
+
+void terminal_putentryat(char *s) {
+   if(*s == '\n') {
+          ssfn_dst.y += ssfn_src->height;
+          ssfn_dst.x = 0;
+   } else
+          ssfn_putc(*s);
+}
+
+void terminal_putchar(char c) {
+   terminal_putentryat(c);
+}
+
+void terminal_write(const char* data, size_t size) {
+   for (size_t i = 0; i < size; i++)
+      terminal_putchar(data[i]);
+}
+
+void terminal_writestring(const char* data) {
+   terminal_write(data, strlen(data));
+}
