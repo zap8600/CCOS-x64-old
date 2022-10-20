6c496c93e31685d16e15e595c891da500b2b7023
zap8600 <95487746+zap8600@users.noreply.github.com>
2022-10-20 15:26:53 +0000

    modified:   kernel/Makefile 	modified:   kernel/arch/x86_64/linker.ld 	modified:   kernel/arch/x86_64/tty.c

--- kernel/arch/x86_64/tty.c
+++ kernel/arch/x86_64/tty.c
@@ -8,40 +8,6 @@
 #include <kernel/tty.h>
 
 extern uint8_t fb;
-<<<<<<< HEAD
-extern volatile unsigned char _binary_console_sfn_start;
-
-void terminal_initialize(void) {
-   ssfn_src = &_binary_console_sfn_start;      /* the bitmap font to use */
-   ssfn_dst.ptr = &fb;                  /* framebuffer address and bytes per line */
-   ssfn_dst.p = 4096;
-   ssfn_dst.fg = 0xFFFFFFFF;                   /* colors, white on black */
-   ssfn_dst.bg = 0;
-   ssfn_dst.x = 100;                           /* coordinates to draw to */
-   ssfn_dst.y = 200;
-}
-
-void terminal_putentryat(char *s) {
-   if(*s == '\n') {
-          ssfn_dst.y += ssfn_src->height;
-          ssfn_dst.x = 0;
-   } else
-          ssfn_putc(*s);
-}
-
-void terminal_putchar(char c) {
-   terminal_putentryat(c);
-}
-
-void terminal_write(const char* data, size_t size) {
-   for (size_t i = 0; i < size; i++)
-      terminal_putchar(data[i]);
-}
-
-void terminal_writestring(const char* data) {
-   terminal_write(data, strlen(data));
-}
-=======
 extern volatile unsigned char _binary_unifont_sfn_start;
 
 void terminal_initialize(void) {
@@ -74,4 +40,3 @@ void terminal_write(const char* data, size_t size) {
 void terminal_writestring(const char* data) {
 	terminal_write(data, strlen(data));
 }
->>>>>>> 9a9b1887c3082e2334d9f5f99826d2af0e89420b
