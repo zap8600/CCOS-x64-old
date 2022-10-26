cbc045d319c6e6c0f7ebe8b38eaaf2351d13b146
zap8600 <95487746+zap8600@users.noreply.github.com>
2022-10-26 18:51:14 -0500

    modified:   kernel/kernel/kernel.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -5,8 +5,11 @@
 void kernel_main() {
 	terminal_initialize();
 	init_idt();
-	terminal_write("beans!\n");
+	terminal_write("Cotton Candy OS\n");
+	terminal_write("WIP\n");
+	terminal_write("Hooray! The IDT was succesfully made!\n");
 	while (1) {}
 	asm volatile ("int $0x3");
+	terminal_write("Hooray! ISRs are working!\n");
 	while (1) {}
 }
