7d2b893abe27d89c8eed5bdef95f3d9908010e7d
zap8600 <95487746+zap8600@users.noreply.github.com>
2022-10-28 15:27:16 +0000

    new file:   kernel/arch/x86_64/gdt.c 	new file:   kernel/include/kernel/gdt.h

---
+++ kernel/arch/x86_64/gdt.c
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+#include <kernel/tty.h>
+#include <kernel/io.h>
+#include <kernel/gdt.h>
---
+++ kernel/include/kernel/gdt.h
@@ -0,0 +1,24 @@
+#include <stdint.h>
+
+typedef struct tss_entry {
+	uint32_t reserved_0;
+	uint64_t rsp[3];
+	uint64_t reserved_1;
+	uint64_t ist[7];
+	uint64_t reserved_2;
+	uint16_t reserved_3;
+	uint16_t iomap_base;
+} __attribute__ ((packed)) tss_entry_t;
+
+typedef struct {
+	uint16_t limit_low;
+	uint16_t base_low;
+	uint8_t base_middle;
+	uint8_t access;
+	uint8_t granularity;
+	uint8_t base_high;
+} __attribute__((packed)) gdt_entry_t;
+
+
+
+void init_gdt();
