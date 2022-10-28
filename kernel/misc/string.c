e48631f3210cfd8671fb0be6392a7e823a9109db
zap8600 <95487746+zap8600@users.noreply.github.com>
2022-10-28 16:00:02 +0000

    new file:   kernel/include/kernel/string.h 	new file:   kernel/misc/string.c

---
+++ kernel/misc/string.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+#include <kernel/string.h>
+
+void* kmemcpy(void* restrict dstptr, const void* restrict srcptr, size_t size) {
+	unsigned char* dst = (unsigned char*) dstptr;
+	const unsigned char* src = (const unsigned char*) srcptr;
+	for (size_t i = 0; i < size; i++)
+		dst[i] = src[i];
+	return dstptr;
+}
