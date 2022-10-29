e452634055b4b8c31b2fbdc930d037d198a9ede5
zap8600 <95487746+zap8600@users.noreply.github.com>
2022-10-28 19:51:47 -0500

    modified:   .vscode/settings.json 	modified:   kernel/arch/x86_64/gdt.c 	modified:   kernel/arch/x86_64/linker.ld 	modified:   kernel/kernel/kernel.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -6,11 +6,10 @@
 void kernel_main() {
 	init_gdt();
 	terminal_initialize();
-	init_idt();
 	terminal_write("Cotton Candy OS\n");
 	terminal_write("WIP\n");
-	terminal_write("Is the IDT working?\n");
+	/* terminal_write("Is the IDT working?\n");
 	asm volatile ("int $0x3");
-	terminal_write("Hooray! The IDT is working!\n");
+	terminal_write("Hooray! The IDT is working!\n"); */
 	while (1) {}
 }
