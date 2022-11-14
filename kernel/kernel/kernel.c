1b93eb8f33f7832a2224c34ff45a3df6271cd686
zap8600 <95487746+zap8600@users.noreply.github.com>
2022-11-14 20:40:00 +0000

    modified:   kernel/include/kernel/mem.h 	modified:   kernel/kernel/kernel.c 	modified:   kernel/kernel/mem.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -10,9 +10,6 @@
 void _kernel_end(void);
 uintptr_t vaddr = (uintptr_t) &_kernel_end;
 
-KHEAPBM kheap;
-char *ptr;
-
 static volatile struct limine_memmap_request memmap_request = {
     .id = LIMINE_MEMMAP_REQUEST,
     .revision = 0
@@ -27,15 +24,9 @@ void kernel_main()
     printf("Second memory-mapped entry type: %d\n", memmap_entry->type);
     printf("Second memory-mapped entry base: %x\n", memmap_entry->base);
     printf("Second memory-mapped entry length: %x\n", memmap_entry->length);
-	kheapinit(&kheap);
-	kaddblock(&kheap, memmap_entry->base, 0x1000, 16);
 	printf("Cotton Candy OS\n");
 	printf("WIP\n");
 	breakpoint;
 	printf("ISRs installed and working.\n");
-	ptr = (char*)kmalloc(&kheap, 256);
-	printf("Pointer to allocated memory: %x\n", &ptr);
-	kfree(&kheap, ptr);
-	printf("Allocated memory has been freed.\n");
 	while (1) {}
 }
