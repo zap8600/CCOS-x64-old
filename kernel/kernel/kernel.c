f4bf50bc9dc9e653ed32c5b7c174809fe475b22a
zap8600 <95487746+zap8600@users.noreply.github.com>
2022-12-14 19:29:37 +0000

    modified:   kernel/arch/x86_64/boot.S 	modified:   kernel/arch/x86_64/linker.ld 	modified:   kernel/kernel/kernel.c 	modified:   kernel/kernel/tty.c 	modified:   kernel/misc/stdio.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,10 +1,33 @@
+/* #include <kernel/limine.h> */
 #include <kernel/tty.h>
 #include <kernel/io.h>
 #include <kernel/gdt.h>
 #include <kernel/ints.h>
 #include <kernel/dbg.h>
+#include <kernel/stdio.h>
+
+/*
+static volatile struct limine_memmap_request memmap_request = {
+    .id = LIMINE_MEMMAP_REQUEST,
+    .revision = 0
+};
+*/
 
 void kernel_main()
 {
+	/*
+	init_gdt();
+	terminal_initialize();
+	init_ints();
+	struct limine_memmap_entry *memmap_entry = memmap_request.response->entries[1];
+    printf("Second memory-mapped entry type: %d\n", memmap_entry->type);
+    printf("Second memory-mapped entry base: %x\n", memmap_entry->base);
+    printf("Second memory-mapped entry length: %x\n", memmap_entry->length);
+	init_mem();
+	printf("Cotton Candy OS\n");
+	printf("WIP\n");
+	breakpoint;
+	printf("ISRs installed and working.\n");
+	*/
 	while (1) {}
 }
--- kernel/kernel/tty.c
+++ kernel/kernel/tty.c
@@ -1,19 +1,45 @@
-#include <stddef.h>
 #include <stdint.h>
 
 #include <kernel/tty.h>
+/* #include <kernel/limine.h> */
 #include <kernel/io.h>
 #include <kernel/string.h>
 
+/*
+static volatile struct limine_terminal_request terminal_request = {
+    .id = LIMINE_TERMINAL_REQUEST,
+    .revision = 0
+};
+
+static void done(void) {
+    for (;;) {
+        __asm__("hlt");
+    }
+}
+*/
+
 void terminal_initialize(void)
 {
+    /*
+    if (terminal_request.response == NULL || terminal_request.response->terminal_count < 1)
+    {
+        done();
+    }
+    */
 }
 
 void terminal_write(const char* data, size_t size)
 {
+    /*
+    struct limine_terminal *terminal = terminal_request.response->terminals[0];
+    terminal_request.response->write(terminal, data, size);
+    */
 }
 
 void terminal_writestring(const char* data)
 {
-    terminal_write(data, strlen(data));
+    /*
+    struct limine_terminal *terminal = terminal_request.response->terminals[0];
+    terminal_request.response->write(terminal, data, size);
+    */
 }
